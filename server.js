a9923f6d4b2b7055a4f403d84c4b563180001c5c
codeCampWithSifat <sayedhossainsifat300@.com>
2025-07-01 12:27:54 +0600

    Cart model are created

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import connectDB from "./config/DB.js";
 import userRoutes from "./routes/UserRoutes.js";
 import productRoutes from "./routes/ProductRoutes.js";
+import cartRoutes from "./routes/CartRoutes.js";
 
 const app = express();
 const port = process.env.PORT;
@@ -22,6 +23,7 @@ app.get("/", (req, res) => {
 // use all the routes
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoutes);
 
 app.listen(port, () => {
   console.log(`Running On Port ${port} Successfully`);
