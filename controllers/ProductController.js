4ae00245d846d64ec342e0651ae79d27b11466d5
codeCampWithSifat <sayedhossainsifat300@.com>
2025-07-01 11:09:43 +0600

    Product Section are done

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -152,11 +152,11 @@ const getAllProducts = async (req, res) => {
 
     //  filter logic
 
-    if (collection && collection.toLocalLowerCase() !== "all") {
+    if (collection && collection.toLocaleLowerCase() !== "all") {
       query.collections = collection;
     }
 
-    if (category && category.toLocalLowerCase() !== "all") {
+    if (category && category.toLocaleLowerCase() !== "all") {
       query.category = category;
     }
 
@@ -180,12 +180,111 @@ const getAllProducts = async (req, res) => {
 
     if (minPrice || maxPrice) {
       query.price = {};
-      if (minPrice) query.price.gte = Number(minPrice);
-      if (maxPrice) query.price.lte = Number(maxPrice);
+      if (minPrice) query.price.$gte = Number(minPrice);
+      if (maxPrice) query.price.$lte = Number(maxPrice);
     }
+
+    if (search) {
+      query.$or = [
+        { name: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
+      ];
+    }
+
+    // Sort Logic
+    let sort;
+    if (sortBy) {
+      switch (sortBy) {
+        case "priceAsc":
+          sort = { price: 1 };
+          break;
+        case "priceDesc":
+          sort = { price: -1 };
+          break;
+        case "popularity":
+          sort = { rating: -1 };
+          break;
+        default:
+          break;
+      }
+    }
+
+    let products = await Product.find(query)
+      .sort(sort)
+      .limit(Number(limit) || 0);
+
+    res.json(products);
   } catch (error) {
     console.error("Server Errror", error);
     res.status(500).json({ message: "Server Error" });
   }
 };
-export { createProduct, updateProduct, deleteProduct, getAllProducts };
+
+const singleProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product Not Found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+const similarProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product Not Found" });
+    }
+    const similarProducts = await Product.find({
+      _id: { $ne: id }, //exclude the current product id
+      category: product.category,
+    }).limit(4);
+    res.json(similarProducts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+// Best seller
+const bestSeller = async (req, res) => {
+  try {
+    const bestSellers = await Product.findOne({}).sort({ rating: -1 });
+    if (bestSeller) {
+      return res.json(bestSellers);
+    } else {
+      res.status(404).json({ message: "No Best Seller Found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+// New Arrivals
+const newArrivals = async (req, res) => {
+  try {
+    // Fetch Latest 8 Product From The Database
+    const newArrival = await Product.find({}).sort({ createdAt: -1 }).limit(8);
+    res.json(newArrival);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+export {
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getAllProducts,
+  singleProduct,
+  similarProduct,
+  bestSeller,
+  newArrivals,
+};
--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { admin, protect } from "../middleware/AuthMiddleware.js";
 import {
+  bestSeller,
   createProduct,
   deleteProduct,
   getAllProducts,
+  newArrivals,
+  similarProduct,
+  singleProduct,
   updateProduct,
 } from "../controllers/ProductController.js";
 
@@ -11,6 +15,10 @@ const router = express.Router();
 
 router.post("/", protect, admin, createProduct);
 router.get("/", getAllProducts);
+router.get("/best-seller", bestSeller);
+router.get("/new-arrivals", newArrivals);
+router.get("/:id", singleProduct);
+router.get("/similar/:id", similarProduct);
 router.put("/:id", protect, admin, updateProduct);
 router.delete("/:id", protect, admin, deleteProduct);
 
